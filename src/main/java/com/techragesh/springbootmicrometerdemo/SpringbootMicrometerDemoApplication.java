f6929062dc4260d3c37c706492122d559a127acb
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2019-07-11 18:37:32 +0200

    Implements example for MicrometerTimingCommandBus.
    See cloudogu/command-bus#10

--- src/main/java/com/techragesh/springbootmicrometerdemo/SpringbootMicrometerDemoApplication.java
+++ src/main/java/com/techragesh/springbootmicrometerdemo/SpringbootMicrometerDemoApplication.java
@@ -3,10 +3,12 @@ package com.techragesh.springbootmicrometerdemo;
 import de.triology.cb.CommandBus;
 import de.triology.cb.decorator.LoggingCommandBus;
 import de.triology.cb.decorator.MicrometerCountingCommandBus;
+import de.triology.cb.decorator.MicrometerTimingCommandBus;
 import de.triology.cb.spring.Registry;
 import de.triology.cb.spring.SpringCommandBus;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -23,10 +25,14 @@ public class SpringbootMicrometerDemoApplication {
 
     @Bean
     public CommandBus commandBus() {
-        return new MicrometerCountingCommandBus(
+        return new MicrometerTimingCommandBus(new MicrometerCountingCommandBus(
                 new LoggingCommandBus(new SpringCommandBus(new Registry(applicationContext))),
                 c -> Counter.builder("command.counter")
                         .description("command execution counter")
+                        .tags("command", c.getSimpleName())
+                        .register(Metrics.globalRegistry)),
+                c -> Timer.builder("command.timer")
+                        .description("command execution timer")
                         .tags("command", c.getSimpleName())
                         .register(Metrics.globalRegistry));
     }
