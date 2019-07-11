63e37b94c675b109e82bf21796955388842686a3
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2019-07-11 18:30:18 +0200

    Implements example for MicrometerCountingCommandBus.
    See cloudogu/command-bus#10

--- src/main/java/com/techragesh/springbootmicrometerdemo/SpringbootMicrometerDemoApplication.java
+++ src/main/java/com/techragesh/springbootmicrometerdemo/SpringbootMicrometerDemoApplication.java
@@ -2,8 +2,11 @@ package com.techragesh.springbootmicrometerdemo;
 
 import de.triology.cb.CommandBus;
 import de.triology.cb.decorator.LoggingCommandBus;
+import de.triology.cb.decorator.MicrometerCountingCommandBus;
 import de.triology.cb.spring.Registry;
 import de.triology.cb.spring.SpringCommandBus;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -20,9 +23,12 @@ public class SpringbootMicrometerDemoApplication {
 
     @Bean
     public CommandBus commandBus() {
-        return new LoggingCommandBus(
-                new SpringCommandBus(new Registry(applicationContext))
-        );
+        return new MicrometerCountingCommandBus(
+                new LoggingCommandBus(new SpringCommandBus(new Registry(applicationContext))),
+                c -> Counter.builder("command.counter")
+                        .description("command execution counter")
+                        .tags("command", c.getSimpleName())
+                        .register(Metrics.globalRegistry));
     }
 
     public static void main(String[] args) {
